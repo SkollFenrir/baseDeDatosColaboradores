f0b0bfb57238e3bced25f0940abab3e1e417a8a0
Diego H <diegoadonis2@gmail.com>
2023-06-04 22:55:34 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Format from './Components/Format/Format';
+import NavbarP from './Components/NavbarP/NavbarP';
+import List from './Components/List/List';
+import { BaseColaboradores } from './data/base';
+import { useState } from 'react';
+
+function App() {
+  const [listCoworker, setListCoworker] = useState (BaseColaboradores);
+  const [savedList, setSavedList] = useState();
+  const handlerClickAdd = (e) => {
+    setListCoworker ([...listCoworker, {id: listCoworker.length + 1, ...e}]);
+    saved();
+  };
+  const saved = () => {
+    setSavedList ([...listCoworker])};
+  const handlerSearch = (filteredSearch) => {
+    const onRender = filteredSearch.length === 0 ? savedList : filteredSearch;
+    setListCoworker ([...onRender]);
+  };
+  return (
+    <div className="App">
+      <NavbarP onChangeSearch= {(e) => handlerSearch (e) } list={listCoworker}/>
+      <article>
+        <Format onClickAddCoworker = {(e) => handlerClickAdd(e)} />
+        <hr></hr>
+        <h2 className='h2'>Listado de Colaboradores</h2>
+        <List list={listCoworker}/>
+      </article>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/List/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+//La lista se le entrega desde el componente padre, simplifica la logica y vuelve el componente reutilizable.
+function List({list}) {    
+return (
+    <ListGroup variant="flush">
+      {list.map(coworker => <ListGroup.Item key={coworker.id} >{coworker.nombre} - {coworker.correo}</ListGroup.Item> )} 
+    </ListGroup>
+);
+};
+
+export default List;
